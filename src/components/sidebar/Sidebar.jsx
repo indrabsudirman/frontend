a01b6493c8f4d0e35ec895ca4a329e2458551df0
indrabsudirman <indrabsudirman@gmail.com>
2024-02-11 23:22:15 +0700

    Update Home and added sidebar header

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { SidebarHeader } from "./header";
+
+export default function Sidebar() {
+  return (
+    <div className="w-[25%] h-full select-none">
+      {/* 30% for set width the sidebar */}
+      {/* sidebar header */}
+      <SidebarHeader />
+    </div>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Sidebar } from "../components/sidebar";
 
 export default function home() {
-  return <div>home</div>;
+  return (
+    <div className="min-h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
+      {/* you can remove py-[19px] to remove padding top */}
+      {/* container */}
+      <div className="container min-h-screen flex">
+        {/* sidebar */}
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
