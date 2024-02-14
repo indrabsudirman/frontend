72a08d360a8727eab1dc14872978fca9f7504925
indrabsudirman <indrabsudirman@gmail.com>
2024-02-14 20:22:37 +0700

    update: json manifest and fix logic token

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route
             exact
             path="/login"
-            element={token ? <Login /> : <Navigate to="/" />}
+            element={!token ? <Login /> : <Navigate to="/" />}
           />
           <Route
             exact
