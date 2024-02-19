f7a7882cf9d7d77a275578a95d7f010eeb7e9ddf
indrabsudirman <indrabsudirman@gmail.com>
2024-02-19 18:13:13 +0700

    Finished whatsapp home

--- src/components/chat/welcome/WhatsappHome.jsx
+++ src/components/chat/welcome/WhatsappHome.jsx
@@ -2,10 +2,23 @@ import { Logo } from "../../../svg";
 
 export default function WhatsappHome() {
   return (
-    <div className="h-full w-[50%] dark:bg-dark_bg_4 select-none border-1 dark:border-1-dark_border_1 border-b-[6px] border-b-green_2">
+    <div className="h-full w-[50%] dark:bg-dark_bg_4 select-none border-1 dark:border-1-dark_border_2 border-b-[6px] border-b-green_2">
       {/* container */}
       <div className="-mt-1.5 w-full h-full flex flex-col gap-y-8 items-center justify-center">
-        <Logo />
+        <span>
+          <Logo />
+        </span>
+        {/*infos*/}
+        <div className="mt-1 text-center space-y-[12px]">
+          <h1 className="text-[32px] dark:text-dark_text_4 font-extralight">
+            Whatsapp Web
+          </h1>
+          <p className="text-sm dark:text-dark_text_3">
+            Send and received messages without keep your phone online.
+            <br />
+            Use Whatsapp on up to 4 linked devices and 1 phone at the some time.
+          </p>
+        </div>
       </div>
     </div>
   );
