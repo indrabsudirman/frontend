82518b850f92d5b62f098b263cb2b11c886fe297
indrabsudirman <indrabsudirman@gmail.com>
2024-02-27 22:37:12 +0700

    Update Chat Messages and Message component

--- src/components/chat/messages/ChatMessages.jsx
+++ src/components/chat/messages/ChatMessages.jsx
@@ -1,8 +1,10 @@
 import { useSelector } from "react-redux";
+import Message from "./Message";
 
 export default function ChatMessages() {
   // const REACT_APP_DARK_BACKGROUND = process.env;
   const { messages } = useSelector((state) => state.chat);
+  const { user } = useSelector((state) => state.user);
   return (
     <div
       className={`mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-no-repeat bg-cover`}
@@ -11,7 +13,11 @@ export default function ChatMessages() {
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[6%]"></div>
       {/* messages */}
       {messages && messages.map((message) => (
-        <Message message={message} key={message._id} />
+        <Message
+          message={message}
+          key={message._id}
+          me={user._id === message.sender._id} />
+
       ))}
     </div>
   );
--- src/components/chat/messages/Message.jsx
+++ src/components/chat/messages/Message.jsx
@@ -1,5 +1,19 @@
-export default function Message() {
+import moment from "moment";
+
+export default function Message({ message, me }) {
   return (
-    <div>Message</div>
+    <div className={`w-full flex mt-2 space-x-3 max-w-xs 
+${me ? "ml-auto justify-end" : ""}`}>
+      {/*message container*/}
+      <div>
+        <div className={`relative h-full dark:text-dark_text_1 p-2 rounded-lg
+{me ? "bg-green_3" : "dark:bg-dark_bg_2"}`}>
+          {/*message*/}
+          <p className="float-left h-full text-sm pb-5">{message.message}</p>
+          {/*message dare*/}
+          <span className="float-right text-xs pt-6 text-dark_text_5">{moment(message.createdAt).format("HH:mm")}</span>
+        </div>
+      </div>
+    </div>
   )
 }
