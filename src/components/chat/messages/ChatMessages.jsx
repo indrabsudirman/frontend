befb9831dbdeb1b743b948f6e55930e7039ad7f8
indrabsudirman <indrabsudirman@gmail.com>
2024-02-27 08:10:16 +0700

    Add Message Component

--- src/components/chat/messages/ChatMessages.jsx
+++ src/components/chat/messages/ChatMessages.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+
 export default function ChatMessages() {
   // const REACT_APP_DARK_BACKGROUND = process.env;
+  const { messages } = useSelector((state) => state.chat);
   return (
     <div
       className={`mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-no-repeat bg-cover`}
@@ -7,6 +10,9 @@ export default function ChatMessages() {
       {/* container */}
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[6%]"></div>
       {/* messages */}
+      {messages && messages.map((message) => (
+        <Message message={message} key={message._id} />
+      ))}
     </div>
   );
 }
