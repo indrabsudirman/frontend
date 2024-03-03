a8a35cafa9b481e0ec044299635d96166ae3c74c
indrabsudirman <indrabsudirman@gmail.com>
2024-03-03 22:43:17 +0700

    fix :gap in message background chat full now

--- src/components/chat/messages/ChatMessages.jsx
+++ src/components/chat/messages/ChatMessages.jsx
@@ -5,9 +5,12 @@ export default function ChatMessages() {
   // const REACT_APP_DARK_BACKGROUND = process.env;
   const { messages } = useSelector((state) => state.chat);
   const { user } = useSelector((state) => state.user);
+  const bgImage =
+    "https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg";
   return (
     <div
-      className={`mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-no-repeat bg-cover`}
+      className={`bg-no-repeat pb-12 bg-cover bg-center`}
+      style={{ backgroundImage: `url(${bgImage})` }}
     >
       {/* container */}
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
