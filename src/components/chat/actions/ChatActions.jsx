db6d7923429de9f57becdcebbfb8d08d2e8556e3
indrabsudirman <indrabsudirman@gmail.com>
2024-03-04 21:24:30 +0700

    Added send message

--- src/components/chat/actions/ChatActions.jsx
+++ src/components/chat/actions/ChatActions.jsx
@@ -2,10 +2,34 @@ import { SendIcon } from "../../../svg";
 import EmojiPicker from "./EmojiPicker";
 import Input from "./Input";
 import Attachments from "./Attachments";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendMessage } from "../../../features/chatSlice";
+import { ClipLoader } from "react-spinners";
 
 export default function ChatActions() {
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+  const { activeConversation, status } = useSelector((state) => state.chat);
+  const values = {
+    message,
+    convo_id: activeConversation._id,
+    files: [],
+    token,
+  };
+  const sendMessageHandler = async (e) => {
+    e.preventDefault();
+    await dispatch(sendMessage(values));
+    setMessage("");
+  };
+
   return (
-    <form className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none">
+    <form
+      onSubmit={(e) => sendMessageHandler(e)}
+      className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none"
+    >
       {/*container*/}
       <div className="w-full flex items-center gap-x-2">
         {/*emoji and attachment*/}
@@ -14,10 +38,15 @@ export default function ChatActions() {
           <Attachments />
         </ul>
         {/*input*/}
-        <Input />
+        <Input message={message} setMessage={setMessage} />
         {/*send button*/}
-        <button className="btn">
-          <SendIcon className="dark:fill-dark_svg_1" />
+        <button type="submit" className="btn">
+          {" "}
+          {status === "loading" ? (
+            <ClipLoader color="#E9EDEF" size={25} />
+          ) : (
+            <SendIcon className="dark:fill-dark_svg_1" />
+          )}
         </button>
       </div>
     </form>
